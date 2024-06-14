6a6d70a3808686c6abf37ccd6bbc4718ee24f590
Kevin Osvaldo Panes Landa <91098125+kpanes@users.noreply.github.com>
2024-06-14 16:22:22 -0600

    Add files via upload

---
+++ practica05/target/generated-sources/jaxb/mx/uv/t4is/saludos/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2024.04.26 a las 04:04:38 PM CST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "t4is.uv.mx/Saludos")
+package mx.uv.t4is.saludos;
